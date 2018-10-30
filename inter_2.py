169b1ecef2df30511cdd2d96a2ac2ee5d2866f6b
Shaban Daniil <ludovicthefirst@gmail.com>
2018-10-30 23:05:21 +0300

    Update inter_2.py

---
+++ inter_2.py
@@ -0,0 +1,30 @@
+i = 0
+s = 0
+d = 0
+gr = []
+par = []
+while i < 20:
+	print("Say something !")
+	z =  input() 
+	v = input("If you greet me, type 'gr', otherwise 'par'  ")
+	if v == 'gr':
+		if s == 0:
+			gr.append(z)
+		elif s > 0:
+			if z in gr:
+				print("you have already wrote this")
+			else:
+				gr.append(z)
+		s+=1
+	elif v == 'par':
+		if d == 0:
+			par.append(z)
+		elif d > 0 :
+			if z in par:
+				print("you have already wrote this")
+			else:
+				par.append(z)
+		d+=1
+	else:
+		print("I'm buzy right now, let's talk another time")
+	i+=1
