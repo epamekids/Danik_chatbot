053070bdd61080e190cbe650a93449c7aa5ea025
Shaban Daniil <ludovicthefirst@gmail.com>
2018-10-30 23:11:32 +0300

    Update inter_2.py

--- inter_2.py
+++ inter_2.py
@@ -10,20 +10,24 @@ while i < 20:
 	if v == 'gr':
 		if s == 0:
 			gr.append(z)
+			print(z)
 		elif s > 0:
 			if z in gr:
-				print("you have already wrote this")
+				print(z-1, "you have already wrote this")
 			else:
 				gr.append(z)
+				print(z-1)
 		s+=1
 	elif v == 'par':
 		if d == 0:
 			par.append(z)
+			print(z)
 		elif d > 0 :
 			if z in par:
-				print("you have already wrote this")
+				print(z-1, "you have already wrote this")
 			else:
 				par.append(z)
+				print(z-1)
 		d+=1
 	else:
 		print("I'm buzy right now, let's talk another time")
